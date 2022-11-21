2664ec9bcc4f02de70225dd5b637f9044722590f
Linus Hammarberg1 <Linus.hammarberg@gmail.com>
2022-11-21 12:48:53 +0100

    StoryTelling klar

--- game.js
+++ game.js
@@ -1,2 +1,18 @@
+let story = 0;
+let form = document.getElementById("theAdventure");
+let submit = document.getElementById("continueButton");
+let reset = document.getElementById("resetButton");
+let answer = "";
 
 
+
+let storyTelling = {
+    "start": {
+        "question": "You set out on a brand new adventure, where would you like to go?";
+        "answers": {
+            "a": "Forest",
+            "b": "Cave",
+            "c": "Mountain",
+        }
+}
+}
