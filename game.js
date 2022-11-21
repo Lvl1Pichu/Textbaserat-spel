90256c8aacc8ca517ed9c85d426111001dfac841
Linus Hammarberg1 <Linus.hammarberg@gmail.com>
2022-11-21 13:01:52 +0100

    Första funktionen tillagd.

--- game.js
+++ game.js
@@ -14,5 +14,24 @@ let storyTelling = {
             "b": "Cave",
             "c": "Mountain",
         }
+},
+//forest path
+"1_a":{
+    "question": "You decide to go to the forest, and you meet a kind fairy. Do you...",
+    "answers": {
+        "a": "Ask her if she wants to join you on your adventure.",
+        "b": "Swat her away like a fly.",
+        "c": "Wave at her and continue on your way.",
+    }
 }
+}
+
+//Continue Link
+submit.addEventListener("mouseup"), function(){
+    answer = form.querySelectorAll("input[type=radio]: checked")[0].value;
+    if(answer){
+        story++;
+        populateForm(story + "_" + answer);
+        console.log("story time!")
+    }
 }
